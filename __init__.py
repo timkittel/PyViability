27c3b2d1c9d6b7e1600601f81a3d2a89e2c47aa4
Tim <Tim.Kittel@pik-potsdam.de>
2016-09-08 16:11:35 +0200

    add __init__.py but not yet used

---
+++ __init__.py
@@ -0,0 +1,18 @@
+
+
+
+from PyViability import backscaling_grid, \
+    generate_grid, \
+    make_run_function, \
+    plot_points, \
+    plot_areas, \
+    print_evaluations, \
+    topology_classification, \
+    scaled_to_one_sunny, \
+    viability_capture_basin, \
+    viability_kernel
+
+from helper import get_ordered_parameters, get_parameter_order
+
+
+
